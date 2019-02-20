1d61c8d03a8be90636d83083463cc5e66e8fb5c0
De Jong <dejong.re2@gmail.com>
2019-02-20 18:51:30 +0000

    Resolve Mixed Content Error

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -19,6 +19,8 @@ try {
 
 window.axios = require('axios');
 
+window.axios.defaults.baseURL= 'https://laravel-store-app.herokuapp.com/'
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
