d004f392dd2687d2a49e1288daaadf81327793ad
De Jong <dejong.re2@gmail.com>
2019-02-20 01:55:41 +0000

    Resolve Mixed content issue - must be served as HTTPS

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -19,7 +19,7 @@ class ProductTableSeeder extends Seeder
                 tempor incididunt ut labore et dolore magna aliqua consequat.",
     			'units' => 21,
     			'price' => 200.10,
-    			'image' => "http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/mens-better-than-naked-jacket-AVMH_LC9_hero.png"
+    			'image' => "https://www.53degreesnorth.ie/media/catalog/product/cache/image/1000x1000/e9c3970ab036de70892d86c6d221abfe/t/h/the-north-face-mens-dryzzle-gore-tex-jacket-tnf-black-aw18.jpg"
     		],
     		[
     			'name' => "WOMEN'S BETTER THAN NAKED&trade; JACKET",
@@ -27,7 +27,7 @@ class ProductTableSeeder extends Seeder
                 tempor incididunt ut labore et dolore magna aliqua consequat.",
     			'units' => 400,
     			'price' => 1600.21,
-    			'image' => "http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/womens-better-than-naked-jacket-AVKL_NN4_hero.png"
+    			'image' => "https://banner2.kisspng.com/20180927/yko/kisspng-the-north-face-womens-endeavor-thermoball-hooded-the-north-face-women39s-endeavor-thermoball-jac-5bad994fc8cbe1.4729268715381036318225.jpg"
     		],
     		[
     			'name' => "WOMEN'S SINGLE-TRACK SHOE",
@@ -35,7 +35,7 @@ class ProductTableSeeder extends Seeder
                 tempor incididunt ut labore et dolore magna aliqua consequat.",
     			'units' => 37,
     			'price' => 378.00,
-    			'image' => "http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/womens-single-track-shoe-ALQF_JM3_hero.png"
+    			'image' => "https://banner2.kisspng.com/20180405/lrq/kisspng-shoe-hiking-boot-mammut-sports-group-the-north-fac-vibrant-5ac5bb05c87d63.6567165915229079098212.jpg"
     		],
     		[
     			'name' => "Enduro Boa&reg; Hydration Pack",
@@ -43,7 +43,7 @@ class ProductTableSeeder extends Seeder
                 tempor incididunt ut labore et dolore magna aliqua consequat.",
     			'units' => 10,
     			'price' => 21.10,
-    			'image' => "http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/enduro-boa-hydration-pack-AJQZ_JK3_hero.png"
+    			'image' => "https://banner2.kisspng.com/20180608/szy/kisspng-backpack-the-north-face-borealis-hiking-the-north-5b1a8311465c12.1270462115284641452882.jpg"
     		]
     	];
 
