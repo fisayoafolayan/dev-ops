ca339c394480c2b53c77ba8a93415b373fcd60e7
De Jong <dejong.re2@gmail.com>
2019-02-20 17:16:46 +0000

    Test for solving Mixed Content Issues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // if($this->app->environment() == 'production') {
-        //     URL::forceScheme('https');
-        // }
+        if(env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
@@ -26,9 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // if($this->app->environment() == 'production') {
-        //     URL::forceScheme('https');
-        // }
+        //
     }
     
 }
