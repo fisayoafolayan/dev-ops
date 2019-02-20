7e5d2d61f933ba6ac6b6aaa067940e5b8143661a
De Jong <dejong.re2@gmail.com>
2019-02-20 21:01:25 +0000

    Reset to Previous: Solving Mixed Content Issues

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,19 +12,12 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies = '*';
+    protected $proxies;
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var string
      */
-    protected $headers = [
-        Request::HEADER_CLIENT_IP => 'X_FORWARDED_FOR',
-        Request::HEADER_FORWARDED    => null,
-        Request::HEADER_CLIENT_HOST  => null,
-        Request::HEADER_CLIENT_HOST  => 'X_FORWARDED_HOST',
-        Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
-        Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT'
-    ];
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
