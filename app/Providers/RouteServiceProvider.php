dd68f47ee3bfa31e97905a6a66d2279fb136d922
De Jong <dejong.re2@gmail.com>
2019-02-21 00:09:14 +0000

    Reset to Previous: Solving Mixed Content Issues

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -23,7 +23,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \URL::forceScheme('https');
+        //
 
         parent::boot();
     }
@@ -65,8 +65,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-             ->middleware('api')
+        Route::prefix('://api')
+             
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
     }
